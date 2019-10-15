a06c05ee611c07cdddd347bbbe5399f4551795ee
dbcabanes <dborrascabanes@hotmail.com>
2019-10-15 11:13:37 +0200

    Movimiento de los TABS a la carpeta PAGES y eliminado del TAB3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    path: "",
+    loadChildren: () =>
+      // Hemos creado la carpeta PAGES y metido dentro los tabs, por lo tanto tenemos que cambiarle la ruta desde aqui
+      import("./pages/tabs/tabs.module").then(m => m.TabsPageModule)
   }
 ];
 @NgModule({
